1f3e15586cfa8a7ddbb4fa467ed2c84009b57246
Angel Valdes <AngelValdes@hotmail.com>
2015-03-05 12:34:53 -0500

    Variable declarations and assigning values
    Initial variable declarations and initialization with values.

--- Valdes_Angel_Output_Assignment/app.js
+++ Valdes_Angel_Output_Assignment/app.js
@@ -4,5 +4,14 @@
     "use strict";
     // Student: Angel Valdes, Student#: 0002747159, Class: SDI, Term: C201503, Assignment: Creating Variables and Output
 
+    // declare variable and assign string value
+    var gymName = "LA Fitness";
+    // declare variable and assign string value
+    var gymAddress = "12245 SW 88 St";
+    // declare variable and assign numeric value
+    var gymFloors = 2;
+    // declare variable and assign boolean value
+    var gymHasRacquetballCourts = true;
+
 
 })();
