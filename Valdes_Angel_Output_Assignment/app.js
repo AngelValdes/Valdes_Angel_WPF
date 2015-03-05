6793e034cdccb226ff4c4741c7527c18ea115e45
Angel Valdes <AngelValdes@hotmail.com>
2015-03-05 12:30:15 -0500

    Referencing js file from html and setting up js file
    Creating the initial IIFE function on js file and referencing the js
    file from the html file

--- Valdes_Angel_Output_Assignment/app.js
+++ Valdes_Angel_Output_Assignment/app.js
@@ -1,1 +1,6 @@
-﻿
+﻿// Immediately Invoke Function Expression - IIFE (for JavaScript Scoping to avoid variables polluting global environment) 
+(function () {
+    // JavaScript strict mode
+    "use strict";
+
+})();
