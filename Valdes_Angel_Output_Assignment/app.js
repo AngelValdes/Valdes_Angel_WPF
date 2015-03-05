0c5944b6518bfafea17c9ec7ce72ff13e347d38d
Angel Valdes <AngelValdes@hotmail.com>
2015-03-05 12:32:33 -0500

    Initial student and assignment information comments
    student info, course info, assignment info

--- Valdes_Angel_Output_Assignment/app.js
+++ Valdes_Angel_Output_Assignment/app.js
@@ -2,5 +2,7 @@
 (function () {
     // JavaScript strict mode
     "use strict";
+    // Student: Angel Valdes, Student#: 0002747159, Class: SDI, Term: C201503, Assignment: Creating Variables and Output
+
 
 })();
