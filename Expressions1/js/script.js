af52a31043bdeea77a71c6b03c2f4008b9a5526b
Angel Valdes <AngelValdes@hotmail.com>
2015-03-09 15:56:23 -0400

    modulo, order of operations, arrays

--- Expressions1/js/script.js
+++ Expressions1/js/script.js
@@ -22,4 +22,27 @@
     var height = 5;
     var area = (width * height)/2;
     console.log("Area of triangle having width=" + width + " and height=" + height + " is " + area);
+
+    // modulo perator to find the remainder, even numbers module is always 0, no remainder!
+    var remainder = 32 % 2;
+    console.log("remainder=" + remainder);
+
+    // order of operations PEMDAS to calculate average
+    var quiz1 = 87, quiz2 = 100, quiz3 = 60, quiz4 = 80;
+    var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
+    console.log("average=" + average);
+
+    // perimeter
+    var length = 7;
+    var newWidth = 6;
+    var perimeter = length * 2 + newWidth * 2;
+    console.log("perimeter=" + perimeter);
+
+    // arrays in expressions
+    var orangeBins = [234, 567, 883];
+    var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
+    console.log("total=" + total);
+
+    
+
 })();
