cc49de5e14f56a3bc364164e294b927eb5c7f379
Angel Valdes <AngelValdes@hotmail.com>
2015-03-09 15:33:56 -0400

    Template setup and add and subtraction operators

---
+++ Expressions1/js/script.js
@@ -0,0 +1,19 @@
+﻿// Student: Angel Valdes, Student#: 0002747159, Class: SDI, Term: C201503, Assignment: ?
+
+(function () {
+    "use strict";
+    // define and assign value to variable using assignment operator
+    var a = 2; 
+    console.log(a);
+
+    // addition operator
+    // a + 3; // not correct!
+    // a = a + 3; // requires assignment operator to change the value of a
+    a += 3;
+    console.log(a);
+
+    // age example with substraction operator
+    var yearBorn = 1973;
+    var age = 2015 - yearBorn -1;
+    console.log(age);
+})();
