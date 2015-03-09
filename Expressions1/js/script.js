2de8976b164ab22377aa65ae384f588c3c9f3ab3
Angel Valdes <AngelValdes@hotmail.com>
2015-03-09 15:41:36 -0400

    operators multiplication and division

--- Expressions1/js/script.js
+++ Expressions1/js/script.js
@@ -1,19 +1,25 @@
-﻿// Student: Angel Valdes, Student#: 0002747159, Class: SDI, Term: C201503, Assignment: ?
+﻿// Student: Angel Valdes, Student#: 0002747159, Class: SDI, Term: C201503, Assignment: Arithmetic operators?
 
 (function () {
     "use strict";
     // define and assign value to variable using assignment operator
     var a = 2; 
-    console.log(a);
+    console.log("a=" + a);
 
     // addition operator
     // a + 3; // not correct!
     // a = a + 3; // requires assignment operator to change the value of a
     a += 3;
-    console.log(a);
+    console.log("a now is =" + a);
 
     // age example with substraction operator
     var yearBorn = 1973;
     var age = 2015 - yearBorn -1;
-    console.log(age);
+    console.log("Age=" + age);
+
+    // area of a triangle
+    var width = 4;
+    var height = 5;
+    var area = (width * height)/2;
+    console.log("Area of triangle having width=" + width + " and height=" + height + " is " + area);
 })();
