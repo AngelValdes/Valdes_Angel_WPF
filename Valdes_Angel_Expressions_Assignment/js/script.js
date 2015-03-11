5156b2967290267a779d33fbcf53f76d8a8441d5
Angel Valdes <AngelValdes@hotmail.com>
2015-03-11 17:33:37 -0400

    fha decision and mortgage initial calculation

--- Valdes_Angel_Expressions_Assignment/js/script.js
+++ Valdes_Angel_Expressions_Assignment/js/script.js
@@ -12,15 +12,17 @@
     var percentDownPayment = Number(window.prompt("Enter % down payment"));
     // variable array of available number of years mortgage
     var nbrOfYearsAvailable = [10, 15, 30];
-    // variable declaration and assignment for number of years
+    // variable declaration and assignment for number of years chosen by user
     var numberOfYears = Number(window.prompt("Choose number of years between " + nbrOfYearsAvailable[0] + ", " + nbrOfYearsAvailable[1] + ", " + nbrOfYearsAvailable[2] + ":"));
-    // down payment calculation and display
+    // down payment calculation 
     var downPayment = (loanAmount * percentDownPayment) / 100;
-    // 
-    
+    // down payment display
     console.log("Down payment: $" + downPayment);
     // variable declaration and assignment from prompt for FHA
     var isFha = window.prompt("Is this FHA (Y)|(N):");
-
-
+    var pmiAmount = 0;
+    if (isFha.toUpperCase() == "Y") {
+        pmiAmount += loanAmount * 1.5;
+    } 
+    var fullMortgageAmount = // add formula to calculate loan amount and monthly payment
 })();
