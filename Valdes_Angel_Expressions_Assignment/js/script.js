a687079754f24c9905043307242c5dc4853baaee
Angel Valdes <AngelValdes@hotmail.com>
2015-03-12 10:24:01 -0400

    Displaying the final result

--- Valdes_Angel_Expressions_Assignment/js/script.js
+++ Valdes_Angel_Expressions_Assignment/js/script.js
@@ -5,20 +5,23 @@
     // Morgage calculator: It will ask you a couple of questions such as loan amount, interest rate, % downpayment and if you will be using FHA financing or not. It will then calculate and display your downpayment, and monthly principal & Interest amount
    
     // variable declaration and assignment for loan amount
-    var loanAmount = Number(window.prompt("Enter loan amount $:", 230000));
+    var salePrice = Number(window.prompt("Enter sale price $:", 230000));
     // variable declaration and assignment for interest rate
-    var monthlyInterestRate = (Number(window.prompt("Enter interest rate %:", 4.25))/100)/12;
+    var interestRate = Number(window.prompt("Enter interest rate %:", 4.25));
+    var monthlyInterestRate = (interestRate / 100) / 12;
     // variable declaration and assignment for percent down payment
     var percentDownPayment = Number(window.prompt("Enter down payment %", 3.5));
+    // calculate loan amount
+    var loanAmount = salePrice;
+    loanAmount -= (salePrice * percentDownPayment)/100;
     // variable array of available number of years mortgage
     var nbrOfYearsAvailable = [10, 15, 30];
 
     // variable declaration and assignment for number of months chosen by user
-    var payments = 12 * Number(window.prompt("Choose number of years between " + nbrOfYearsAvailable[0] + ", " + nbrOfYearsAvailable[1] + ", " + nbrOfYearsAvailable[2] + ":", 30));
+    var nbrOfYearsEntered = Number(window.prompt("Choose number of years between " + nbrOfYearsAvailable[0] + ", " + nbrOfYearsAvailable[1] + ", " + nbrOfYearsAvailable[2] + ":", 30));
+    var payments = 12 * nbrOfYearsEntered;
     // down payment calculation 
-    var downPayment = (loanAmount * percentDownPayment) / 100;
-    // down payment display
-    console.log("Down payment: $" + downPayment);
+    var downPayment = (salePrice * percentDownPayment) / 100;
 
     // variable declaration and assignment from prompt for FHA
     var isFha = window.prompt("Is this FHA (Y)|(N):", "Y");
@@ -26,14 +29,14 @@
     var pmiAmount = 0;
     // if Fha then calculate pmi amount
     if (isFha.toUpperCase() === "Y") {
-        pmiAmount += loanAmount * 2.5;
+        pmiAmount += loanAmount * 0.025; // estimated amount
     }
 
     // calculate monthly PrincipalAndInterest
     var calcMonthlyInterest = (1 + monthlyInterestRate);
     var monthlyPrincipalAndInterest = (loanAmount * (monthlyInterestRate * (Math.pow(calcMonthlyInterest, payments)))) / (Math.pow(calcMonthlyInterest, payments) - 1);
 
-    console.log(monthlyPrincipalAndInterest);
-    console.log(monthlyPrincipalAndInterest);
+    console.log("For an input sale price of $" + salePrice + ", monthly interest rate of " + interestRate + "%, a percent down payment of " + percentDownPayment + "%, and a loan for " + nbrOfYearsEntered + " years. \n Your loan amount is $" + loanAmount +  " after a down payment of $" + downPayment + ", your monthly principal + interest amount is: $" + monthlyPrincipalAndInterest + ". \n Your estimated PMI is $" + pmiAmount + ".");
+
 
 })();
