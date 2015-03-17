45927f836cec97afec2b94dbb986d31a9e82d615
Angel Valdes <AngelValdes@hotmail.com>
2015-03-17 14:56:36 -0400

    Fahrenheit to celsius

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -14,4 +14,19 @@
     //-Celsius to Fahrenheit converter
     /* Convert a temperature to either degrees Celsius or degrees Fahrenheit depending on what the user has entered. If the user puts a “C” for the unit, the calculator should convert to Celsius. If the user puts “F” for the unit, the calculator should convert to Fahrenheit. Given: Degrees (in F or C), Unit (a string holding an “F” or a “C”). Result: “The temperature is X degrees Celsius.” Or “The temperature is X degrees Fahrenheit.” test values: 32F is 0C, 100C is 212F, 90F is 32.22C*/
 
+    var temperature = Number(window.prompt("Enter temperature:", 32));
+    var unit = window.prompt("Enter temperature unit C=Celsius | F=Fahrenheit", "F");
+    var result;
+    if (unit.toUpperCase() === "F") {
+        result = ((5 / 9) * (temperature - 32)) + " degrees Celsius";
+    } else {
+        result = (((9 / 5) * temperature) + 32) + " degrees Fahrenheit";
+    }
+    console.log("The temperature is " + result);
+
+    /* Last Chance for Gas!
+    A driver has to determine if they can make it across the desert with their current fuel.  They are about to past the last gas station for the next 200 miles and they need to determine whether they should stop now for gas or not. Given:
+   Gas efficiency of the car (in mpg), Gauge reading of the gas tank (in %), Car’s gas tank capacity (in gallons). Result To Print Out: “Yes, you can make it without stopping for gas!” or “You only have X gallons of gas in your tank, better get gas now while you can!”*/
+
+
 })();
