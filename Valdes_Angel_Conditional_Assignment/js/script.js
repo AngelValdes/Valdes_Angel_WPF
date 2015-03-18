0b096872a186926a85cad6060299edf4ec28d9d3
Angel Valdes <AngelValdes@hotmail.com>
2015-03-18 13:08:34 -0400

    Initial variable declaration and validation

--- Valdes_Angel_Conditional_Assignment/js/script.js
+++ Valdes_Angel_Conditional_Assignment/js/script.js
@@ -4,5 +4,16 @@
     "use strict";
     // Tip calculator
    
-   
+    // variable declarations 
+    var dinnerCost = 0, dinneIn = "", serviceQuality = "", tipPercent = 0, additionalTip = 0, tipAmount = 0, suggestedTip = 0, totalCharge = 0;
+    // dinnerCost prompt, validation, and variable assignment
+    do { //loop
+        dinnerCost = Number(window.prompt("Enter dinner cost:", 50));
+    } while (dinnerCost <= 0 || isNaN(dinnerCost)) // validate values positive numbers only
+    // dinneIn prompt, validation, and variable assignment   
+    do {//loop
+        dinneIn = window.prompt("Dinner In? (y=Yes) (n=No):", "y");
+    } while (!(dinneIn in { "y": 0, "Y": 0, "n": 0, "N": 0 }))// validate values in list
+
+    
 })();
