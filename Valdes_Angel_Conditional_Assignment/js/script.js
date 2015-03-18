4b76cb1e6065981466069e2abf4a276a1a1ff5b5
Angel Valdes <AngelValdes@hotmail.com>
2015-03-18 13:06:53 -0400

    Conditional assignment Project setup

---
+++ Valdes_Angel_Conditional_Assignment/js/script.js
@@ -0,0 +1,8 @@
+﻿// Student: Angel Valdes, Student#: 0002747159, Class: SDI, Term: C201503, 03/10/2015, Assignment: Conditional assignment
+
+(function () {
+    "use strict";
+    // Tip calculator
+   
+   
+})();
