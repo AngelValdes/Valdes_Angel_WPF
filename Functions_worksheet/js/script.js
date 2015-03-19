c3d75cc3f2c115bf9efba3945c433bafc2b2c1d8
Angel Valdes <AngelValdes@hotmail.com>
2015-03-19 09:47:09 -0400

    comment on first exercise

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -1,6 +1,12 @@
-﻿// Student: Angel Valdes, Student#: 0002747159, Class: SDI, Term: C201503, Assignment: ?
+﻿// Student: Angel Valdes, Student#: 0002747159, Class: SDI, Term: C201503, Assignment: Functions Worksheet
 
 (function () {
     "use strict";
+    // Calculate the circumference of a circle.
+    /* Parameter(s) for function: Radius of the circle
+       Return: Circumference of the circle
+       Result to print to the console: “The circumference of the circle is X”
+     */
+
 
 })();
