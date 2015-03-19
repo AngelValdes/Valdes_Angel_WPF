292c060bd1d3f620f82f16d62bfe21bca5f84655
Angel Valdes <AngelValdes@hotmail.com>
2015-03-19 10:18:53 -0400

    function definition

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -2,17 +2,22 @@
 
 (function () {
     "use strict";
-    // Calculate the circumference of a circle. C=2*Pi*r
+    // Calculate the circumference of a circle. C=2*PI*r
     /* Parameter(s) for function: Radius of the circle
        Return: Circumference of the circle
        Result to print to the console: “The circumference of the circle is X”
      */
     // variable definitions
-    var radius = 0, result = 0, message = "The circumference of the circle is ";
+    var radius = 0, PI = 3.14,  circumference = 0, message = "The circumference of the circle is ";
     // radius prompt and validation
     do { //loop
         radius = Number(window.prompt("Enter radius of a circle:", 10));
     } while (radius <= 0 || isNaN(radius))
+    // function definition
+    function calculateCircumference(r) {
+        return 2 * PI * r;
+    }
+   
 
 
 })();
