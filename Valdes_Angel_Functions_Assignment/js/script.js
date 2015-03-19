9190210bc9b203f1c15a412ce62176805614e086
Angel Valdes <AngelValdes@hotmail.com>
2015-03-19 16:02:24 -0400

    Prompt for 6 numbers and validating numbers

--- Valdes_Angel_Functions_Assignment/js/script.js
+++ Valdes_Angel_Functions_Assignment/js/script.js
@@ -2,6 +2,17 @@
 
 (function () {
     "use strict";
-    // Florida lottery game
+    // Florida lottery game (6 numbers between 1 and 53)
+    // variables declaration
+    var playerNumbers = [], lastNumberPlayed = 0;
+    // validating numbers
+    for (var i = 1; i <= 6; i++) {
+
+        do { // loop validation
+            lastNumberPlayed = Number(window.prompt("[From 1 to 53], Enter lottery number ("+ i + "):", 10));
+        } while (lastNumberPlayed < 1 || isNaN(lastNumberPlayed || lastNumberPlayed > 53));
+        playerNumbers.push(lastNumberPlayed);
+    };
+    console.log("ready");
 
 })();
