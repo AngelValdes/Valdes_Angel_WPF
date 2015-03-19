49f0cc195709b8513fbb946779eeb87c9a8ae009
Angel Valdes <AngelValdes@hotmail.com>
2015-03-19 10:30:29 -0400

    Example added

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -2,6 +2,17 @@
 
 (function () {
     "use strict";
+    //Example: Calculate the area of a rectangle given the width and height of the rectangle.
+    //Calculate the Area of a Rectangle
+    //var width = 5;
+    //var height = 6;
+    //var area = calculateArea(width, height);
+    //console.log("The Area of the Rectangle is " + area);
+    //function calculateArea(w, h )
+    //{
+    //    return w *h;
+    //} 
+
     // Calculate the circumference of a circle. C=2*PI*r
     /* Parameter(s) for function: Radius of the circle
        Return: Circumference of the circle
