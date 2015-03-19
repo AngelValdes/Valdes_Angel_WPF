8b1e32dd19d8c35ea192f09c8dc071a1015db1f6
Angel Valdes <AngelValdes@hotmail.com>
2015-03-19 16:09:26 -0400

    generate winning numbers title

--- Valdes_Angel_Functions_Assignment/js/script.js
+++ Valdes_Angel_Functions_Assignment/js/script.js
@@ -4,15 +4,18 @@
     "use strict";
     // Florida lottery game (6 numbers between 1 and 53)
     // variables declaration
-    var playerNumbers = [], lastNumberPlayed = 0;
-    // validating numbers
+    var playerNumbers = [], lastNumberPlayed = 0, winningNumbers = [];
+    // Prompt for 6 numbers and validating numbers
     for (var i = 1; i <= 6; i++) {
 
         do { // loop validation
             lastNumberPlayed = Number(window.prompt("[From 1 to 53], Enter lottery number ("+ i + "):", 10));
         } while (lastNumberPlayed < 1 || isNaN(lastNumberPlayed || lastNumberPlayed > 53));
+        // add player numbers to array
         playerNumbers.push(lastNumberPlayed);
     };
+    // generate winning numbers
+
     console.log("ready");
 
 })();
