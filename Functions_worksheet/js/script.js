092155eaa4c6b1be90862c8776a5e81749616fbd
Angel Valdes <AngelValdes@hotmail.com>
2015-03-19 10:14:21 -0400

    variable definitions

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -2,11 +2,14 @@
 
 (function () {
     "use strict";
-    // Calculate the circumference of a circle.
+    // Calculate the circumference of a circle. C=2*Pi*r
     /* Parameter(s) for function: Radius of the circle
        Return: Circumference of the circle
        Result to print to the console: “The circumference of the circle is X”
      */
+    // variable definitions
+    var radius = 0, result = 0, message = "The circumference of the circle is ";
+   
 
 
 })();
