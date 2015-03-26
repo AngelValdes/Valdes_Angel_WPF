58a508c675113448f2bb7935085e13806729ca25
Angel Valdes <AngelValdes@hotmail.com>
2015-03-26 16:45:28 -0400

    variable declarations

--- Valdes_Angel_Functions_Assignment/js/script.js
+++ Valdes_Angel_Functions_Assignment/js/script.js
@@ -4,5 +4,8 @@
     "use strict";
     // Lottery games Florida lottery (6 numbers between 1 and 53) || Powerball (5 numbers between 1 and 59 + 1 number between 1 and 35)
 
+    // variables declaration
+    var lotteryGame, lotteryType, rangeMin = 1, flRangeMax = 53, flRangeQty = 6, pbRangeMax = 59, pbRangeQty = 5, pbMax = 35, pbQty = 1, rangeMax = 0, rangeQty = 0, playerNumbers = [], pbPlayerNumber, winningNumbers = [], pbWinningNumber, numbersRangeMatching, pbMatch = false, winner = false, resultMessage;
+
    
 })();
