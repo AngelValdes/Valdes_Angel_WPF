b8271bd3bdde04ca614cdd02d1d0930ebfda6fcc
Angel Valdes <AngelValdes@hotmail.com>
2015-03-26 16:50:50 -0400

    display result and function definitions

--- Valdes_Angel_Functions_Assignment/js/script.js
+++ Valdes_Angel_Functions_Assignment/js/script.js
@@ -38,5 +38,121 @@
     winner = checkWinner(numbersRangeMatching, pbMatch);
     // calling function to construct message
     resultMessage = constructMessage(winner, lotteryType, numbersRangeMatching);
-   
+    // display final message
+    console.log(resultMessage);
+
+    // function definition to prompt for x quantity numbers, between min and max and return an array
+    function promptNumbers(min, max, quantity) {
+        var numbers = [], promptMessage, newNumber;
+        // Prompt for x numbers and validating numbers
+        while (numbers.length < quantity) {
+            // construct message to display on prompt 
+            if (quantity == 1) {
+                promptMessage = "[1 to 35] Enter Power Ball number:";
+            } else {
+                promptMessage = "[" + min + " to " + max + "] Enter one number to play: \n Already entered: ";
+                // construct message to display already entered numbers
+                for (var number in numbers) {
+                    promptMessage += numbers[number] + " ";
+                }
+            }
+            // prompt and validate new entered number, suggest random number
+            do {
+                newNumber = Number(window.prompt(promptMessage, Math.floor(Math.random() * ((max - min) + 1) + min)));
+            } while (newNumber < min || isNaN(newNumber || newNumber > max));
+            // is entered number in array already? if it is ask for new number for same position
+            if (numbers.indexOf(newNumber) < 0) {
+                // add player numbers to array
+                numbers.push(newNumber);
+            }
+        };
+        return numbers;
+    }
+
+    // function definition to generate x quantity of winning numbers, between min and max, and unique
+    function generateWinningNumbers(min, max, quantity) {
+        var numbers = [];
+        while (numbers.length < quantity) {
+            // generate new number
+            var newNumber = Math.floor(Math.random() * ((max - min) + 1) + min);
+            // is number in array already?
+            if (numbers.indexOf(newNumber) < 0) {
+                // add number to array
+                numbers.push(newNumber);
+            }
+        };
+        return numbers; // return array of unique numbers
+    }
+
+    // function definition to find matches of winning numbers
+    function findMatches(winning, player) {
+        var matching = [];
+        for (var i = 0; i < winning.length; i++) {
+            // look for each played number in winning array, if found add to matching array
+            if (winning.indexOf(player[i]) >= 0) {
+                matching.push(player[i]);
+            }
+        }
+        return matching;
+    }
+
+    // function definition to checkWinner
+    function checkWinner(matching, pbMatched) {
+        var result = false; // not a winner by default
+        if (matching.length == rangeQty) { // possible winner
+            if (lotteryType == "FloridaLotto" || pbMatched) { // winner
+                result = true;
+            }
+        }
+        return result;
+    }
+    // function definition to construct message
+    function constructMessage(isWinner, lotType, matching) {
+        var newMessage = "";
+        if (isWinner) { // winner message
+            newMessage = "You are a winner!";
+        } else { // not a winner message
+            newMessage = "ohhh noo, you got " + matching.length;
+            switch (matching.length) { // display message based on how many matches
+                case 0:
+                    newMessage += " match.";
+                    break;
+                case 1:
+                    newMessage += " match. it was ";
+                    break;
+                default:
+                    newMessage += " matches. They were ";
+                    break;
+            }
+            // add actuall matching numbers to message
+            for (var match in matching) {
+                newMessage += matching[match] + " ";
+            }
+        }
+        // display winning numbers
+        newMessage += "\n the winning numbers are: ";
+        for (var winningNumber in winningNumbers) {
+            newMessage += winningNumbers[winningNumber] + " ";
+        }
+        // add powerball if applicable
+        if (lotType == "Powerball") {
+            newMessage += " with Powerball " + pbWinningNumber;
+        }
+        // display player numbers
+        newMessage += "\n the player numbers are: ";
+        for (var playerNumber in playerNumbers) {
+            newMessage += playerNumbers[playerNumber] + " ";
+        }
+        // add powerball if applicable
+        if (lotType == "Powerball") {
+            newMessage += " with Powerball " + pbPlayerNumber;
+        }
+        return newMessage; // return constructed message
+    }
+
+    /*
+     * ohhh noo, you got 1 match. it was 29 
+     * the winning numbers are: 4 49 1 29 32 16 
+     * the player numbers are: 29 3 35 31 46 6 
+     */
 })();
